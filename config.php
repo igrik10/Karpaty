7d1e34943bfe54b236836061952a4955f6669d15
dell <igrik10@gmail.com>
2015-03-10 18:06:27 +0200

    Created test drive form, added global Admin mail config, localized bootstrap datepicker, added form sending to admin mail

---
+++ config.php
@@ -0,0 +1,3 @@
+<?php
+    const ADMIN_MAIL = "<admin_karpatyautocenter@ukr.net>";
+    const MAIL_HEADERS = "MIME-Version: 1.0\r\n Content-type: text/html; charset=utf-8\r\n From: <noreply@karpaty-autocenter.com.ua>\r\n";
--- pre_order.php
+++ pre_order.php
@@ -2,7 +2,8 @@
 include 'bd.php';
 include 'kurs.php';
 include 'component/component.php';
-include 'component/meta.php'; ?>
+include 'component/meta.php';
+include 'config.php'?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -180,37 +181,58 @@ include 'component/meta.php'; ?>
         <div class="col-md-3" id="col-md-3-pre_order">
             <form class="form-horizontal">
                 <div class="form-group">
-                    <label for="inputEmail3" class="col-sm-2 control-label">Имя</label>
+                    <label for="inputName3" class="col-sm-2 control-label">Имя</label>
                     <div class="col-sm-10">
-                        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                        <input type="text" class="form-control" id="inputName3" name="inputName" placeholder="Имя">
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="inputPassword3" class="col-sm-2 control-label">Фамилия</label>
+                    <label for="inputLastName3" class="col-sm-2 control-label">Фамилия</label>
                     <div class="col-sm-10">
-                        <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                        <input type="text" class="form-control" id="inputLastName3" name="inputLastName" placeholder="Фамилия">
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
                     <div class="col-sm-10">
-                        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
-                    <div class="col-sm-10">
-                        <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                        <input type="email" class="form-control" id="inputEmail3" name="inputEmail" placeholder="Email">
                     </div>
                 </div>
                 <div class="form-group">
                     <div class="col-sm-offset-2 col-sm-10">
-                        <button type="submit" class="btn btn-default">Отправить</button>
+                        <button type="submit" class="btn btn-default" name="pre_order">Отправить</button>
                     </div>
                 </div>
             </form>
         </div>
     </div>
+
+
+    <?php
+    if(isset($_POST['pre_order'])){
+        $testDriveForm = array(
+            'lastName'  =>  $_POST['inputLastName'],
+            'name'      =>  $_POST['inputName'],
+            'mail'      =>  $_POST['inputMail'],
+        );
+
+        $subject  = "ПРЕДЗАКАЗ";
+
+        $message  = "Предзаказ: \r\n";
+
+        $message .= "\r\n Фамилия: ";
+        $message .= $testDriveForm['lastName'];
+
+        $message .= "\r\n Имя: ";
+        $message .= $testDriveForm['name'];
+
+        $message .= "\r\n E-mail: ";
+        $message .= $testDriveForm['mail'];
+
+        mail(ADMIN_MAIL, $subject, $message, MAIL_HEADERS);
+    }
+    ?>
+
 
 <div class="span12" style="color: #666;margin-top: 10px;font-size: 11px;">
 <span style="float: right;">"КарпатыАвтоцентр" | г. Черновцы, ул.Энергетическая 2 В | +38 (0372) 543-888, +38 (050) 374-40-08, +38 (067) 373-57-00
